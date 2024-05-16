b640fcf34c83708f079af99929ecaf06ce759cbb
wayme23 <152910772+wayme23@users.noreply.github.com>
2024-05-16 14:33:04 +0300

    Update Var07_Couple.cs

--- ClassLibrary1/Var07_Couple.cs
+++ ClassLibrary1/Var07_Couple.cs
@@ -5,8 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ClassLibrary1
-{
-    
+{    
     internal class Var07_Couple
     {
         internal static Var07_Couple ToString(string value)
